66ad3658364119b2149ac021c7b7a22282548a16
Mark <marklviv96@gmail.com>
2017-07-18 20:33:46 +0300

    Possibility to delete tasklists

--- app/controllers/tasklists_controller.rb
+++ app/controllers/tasklists_controller.rb
@@ -5,6 +5,13 @@ class TasklistsController < ApplicationController
 		redirect_to project_path(@project)
 	end
 
+	def destroy
+		@project = Project.find(params[:project_id])
+		@tasklist = @project.tasklists.find(params[:id])
+		@tasklist.destroy
+		redirect_to project_path(@project)
+	end
+
 	private
 	def tasklist_params
 		params.require(:tasklist).permit(:name, :project_id)
